ea9d3c27eb69d767986bb89721d4e1b58feaff1f
alamin500 <nalamin500@gmail.com>
2021-09-22 12:33:03 +0600

    first commit

---
+++ src/Components/Utilities/storage.js
@@ -0,0 +1,9 @@
+export function add(num1, num2){
+    return num1 + num2;
+}
+export function multiply(num1, num2){
+    return num1 * num2;
+}
+function substract(num1, num2){
+    return num1 - num2;
+}
