a221ecb0297686a17392355c4b83a4a337fa9535
alamin500 <nalamin500@gmail.com>
2021-09-23 07:30:43 +0600

    j

--- src/Utilities/fakedb.js
+++ src/Utilities/fakedb.js
@@ -1,13 +1,20 @@
 // use localstorage as your db for now 
 const addToDb = id => {
-    const exists = localStorage.getItem(id);
-    console.log(exists);
+    const exists = localStorage.getItem('shopping_cart');
+    let shopping_cart = {};
     if(!exists){
-        localStorage.setItem(id, 1);
+        shopping_cart[id] = 1;
     }
     else{
-        const newCount = parseInt(exists) + 1;
-        localStorage.setItem(id, newCount)
-    }
+        shopping_cart = JSON.parse(exists);
+        if(shopping_cart[id]){
+            const newCount = shopping_cart[id] + 1;
+            shopping_cart[id] = newCount;
+        }
+        else{
+            shopping_cart[id] = 1;
+        }
+}
+    localStorage.setItem('shopping_cart', JSON.stringify(shopping_cart));
 }
 export{addToDb}
